8ac0ec0153372c83a69663aec67792bbebee1d93
Dario <darioarias12@gmail.com>
2022-05-26 15:10:16 -0400

    work on some data structures, more problems, and re-tried old problems

---
+++ javaScript/primative/powerOfTwo.js
@@ -0,0 +1,20 @@
+/**
+ * Given a non-negative integer input, return true if input is a power of two.
+ * Return false otherwise.
+ *
+ * Example #1:
+ * Input: 4
+ * Output: true
+ * Explanation: The number 4 is a power of two.
+ *
+ * Example #2:
+ * Input: 17
+ * Output: false
+ * Explanation: The number 17 is not a power of two.
+ *
+ * Constraints:  Your solution should run in O(1) time.
+ */
+const { Shifter: num } = require("../structures/binary");
+const solution = (input) => {};
+
+num.show_binary(2, 1);
