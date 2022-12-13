1a6ebeab8197922fadebd19d7b79ad393100c851
Dario <darioarias12@gmail.com>
2022-12-13 07:58:03 -0500

    Created a Singly Linked List class that supports inserting, searching and removing from the list. The class provides support to adding from the front and back for the list, as well as removing from either sides. There are a couple of dunders that are overloaded as well. We overloaded the following, iter -- makes it easier to print the list, repr -- for debug purposes, str -- to print the list, constains -- allows the user to check if a value is in the list i.e--1 in List--, reversed -- allows the list to be revered, bool -- determines whether or not a list exists.

---
+++ python/leetcode/data_structures/linked_lists.py
@@ -0,0 +1,142 @@
+from __future__ import annotations
+
+import typing
+
+from .nodes import DoublyLinkedListNode as DLLNode
+from .nodes import SinglyLinkedListNode as SLLNode
+
+T = typing.TypeVar("T")
+
+
+class SinglyLinkedList(typing.Generic[T]):
+    def __init__(self, __items: typing.Optional[typing.Iterable[T]] = None) -> None:
+        self._head: typing.Optional[SLLNode[T]] = None
+        self._tail: typing.Optional[SLLNode[T]] = None
+
+        if __items is not None:
+            for item in __items:
+                self.append(item)
+
+    @staticmethod
+    def print_from(head: SLLNode[typing.Any]) -> None:
+        def gen(head: typing.Optional[SLLNode[typing.Any]]) -> typing.Iterator[str]:
+            while head:
+                yield str(head)
+                head = head.next
+
+        print(" -> ".join(gen(head)))
+
+    def push(self, value: T) -> None:
+        """Adds a value at the front of the list."""
+        self._head = SLLNode[T](value, self._head)
+
+        if self._tail is None:
+            self._tail = self._head
+
+    def append(self, value: T) -> None:
+        """Adds a value at the end of the list."""
+        if self._tail is None:
+            return self.push(value)
+
+        self._tail.next = SLLNode[T](value)
+        self._tail = self._tail.next
+
+    def insert(self, after: SLLNode, value: T) -> None:
+        """Adds a value after a particular list node."""
+        assert isinstance(after, SLLNode), "After must be a Node"
+
+        if after is self._tail:
+            return self.append(value)
+
+        _, next = after
+        after.next = SLLNode[T](value, next)
+
+    def pop(self) -> typing.Optional[T]:
+        """Removes the value at the front of the list."""
+        if self._head is None:
+            return None
+
+        value, next = self._head
+
+        self._head = next
+
+        if self._head is None:
+            self._tail = None
+
+        return value
+
+    def remove_last(self) -> typing.Optional[T]:
+        """Removes the value at the end of the list."""
+        if self._tail is None:
+            return None
+
+        if self._tail is self._head:
+            return self.pop()
+
+        before_tail: typing.Optional[SLLNode] = None
+        for node in self:
+            if node.next is self._tail:
+                before_tail = node
+                break
+
+        assert isinstance(
+            before_tail, SLLNode
+        ), "the node before tail should always be of type Node"
+
+        value, _ = self._tail
+        before_tail.next = None
+        self._tail = before_tail
+        return value
+
+    def remove(self, after: SLLNode[T]) -> typing.Optional[T]:
+        """Removes a value anywhere in the list."""
+        assert isinstance(after, SLLNode), "After must be of type Node"
+
+        if after.next is None:
+            return None
+
+        if after.next is self._tail:
+            return self.remove_last()
+
+        value, next = after.next
+
+        after.next = next
+        return value
+
+    def __iter__(self) -> typing.Iterator[SLLNode[T]]:
+        current = self._head
+        while current:
+            yield current
+            current = current.next
+
+    def __repr__(self) -> str:
+        return " -> ".join(repr(node) for node in self)
+
+    def __str__(self) -> str:
+        return " -> ".join(str(node) for node in self)
+
+    def __contains__(self, value: T) -> bool:
+        for node_val, _ in self:
+            if node_val == value:
+                return True
+        return False
+
+    def __reversed__(self) -> typing.Iterator[SLLNode[T]]:
+        def _empt_itr() -> typing.Iterator[SLLNode[T]]:
+            yield from ()
+
+        def _reverse(node: SLLNode[T]) -> typing.Iterator[SLLNode[T]]:
+            if node.next is None:
+                yield node
+            else:
+                yield from _reverse(node.next)
+                yield node
+
+        if self._head is None:
+            return _empt_itr()
+        return _reverse(self._head)
+
+    def __bool__(self) -> bool:
+        if self._head is None:
+            return False
+        return True
