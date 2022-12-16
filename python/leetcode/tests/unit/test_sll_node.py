ac3427b035ab3d155b977bef537042c31e41a39e
Dario <darioarias12@gmail.com>
2022-12-15 22:17:53 -0500

    Testing all of the properties that a singly linked list makes avaiable.

--- python/leetcode/tests/unit/test_sll_node.py
+++ python/leetcode/tests/unit/test_sll_node.py
@@ -11,3 +11,19 @@ class TestSLLNode(unittest.TestCase):
     def tearDown(self) -> None:
         del self.vals
         del self.nodes
+
+    def test_properties(self) -> None:
+        n_one, n_two, *_ = self.nodes
+        one, *_ = self.vals
+
+        self.assertEqual(n_one.value, one)
+        self.assertEqual(n_one.val, one)
+        self.assertIs(n_one.next, None)
+
+        setattr(n_one, "value", 2)
+        self.assertEqual(n_one.val, 2)
+        n_one.val = 1
+        self.assertEqual(n_one.val, 1)
+
+        n_one.next = n_two
+        self.assertIs(n_one.next, n_two)
