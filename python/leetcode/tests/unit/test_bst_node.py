c4dba4f1fe8dd504fd2f04b86160f4106c75dee3
Dario <darioarias12@gmail.com>
2022-12-15 22:10:23 -0500

    Defining how to run tests.

--- python/leetcode/tests/unit/test_bst_node.py
+++ python/leetcode/tests/unit/test_bst_node.py
@@ -121,3 +121,7 @@ class TestBSTNode(unittest.TestCase):
         self.assertIs(right, None)
 
     # TODO: test repr and str - first update cls to support str
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
