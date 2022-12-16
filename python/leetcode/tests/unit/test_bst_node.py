0e5e635a2b08e15224847ad02162c23cdacbcf44
Dario <darioarias12@gmail.com>
2022-12-15 22:08:33 -0500

    Testing all properties found in an BSTNode. Making sure that the correct exceptions are raised when needed.

--- python/leetcode/tests/unit/test_bst_node.py
+++ python/leetcode/tests/unit/test_bst_node.py
@@ -25,3 +25,44 @@ class TestBSTNode(unittest.TestCase):
         del self.n_one
         del self.n_two
         del self.n_three
+
+    def test_properties(self) -> None:
+        """makes sure that all set and default values are set correctly"""
+        # Getters
+        value: int = 10
+        node: Node[int] = Node(value)
+
+        self.assertEqual(node.val, value)
+        self.assertEqual(node.value, value)
+
+        self.assertIs(node.left, None)
+        self.assertIs(node.right, None)
+
+        self.assertIsInstance(node.val, int)
+
+        value2: int = 5
+        node2: Node[int] = Node(value2)
+
+        value3: int = 15
+        node3: Node[int] = Node(value3)
+
+        node.left = node2
+        self.assertFalse(node.left is None)
+        self.assertIs(node.left, node2)
+
+        node.right = node3
+        self.assertFalse(node.right is None)
+        self.assertIs(node.right, node3)
+
+        self.assertIs(node.min, node2)
+
+        self.assertRaises(AttributeError, setattr, node, "min", Node(0))
+
+        # setters
+        temp_node = Node(20)
+        self.assertEqual(temp_node.val, 20)
+
+        temp_node.val = 10
+
+        self.assertNotEqual(temp_node.val, 20)
+        self.assertEqual(temp_node.val, 10)
