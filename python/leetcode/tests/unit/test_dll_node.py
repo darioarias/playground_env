687995eec18a03a01b9a6b1917205aa09e2e4fad
Dario <darioarias12@gmail.com>
2022-12-15 22:12:22 -0500

    Setting up the class to test the Doubly Linked List Node. In the setup, we crate a vals tuple containg the values we will be usign to test. We create nodes dynamically for each values as well and stored them in a tuple named nodes.

---
+++ python/leetcode/tests/unit/test_dll_node.py
@@ -0,0 +1,13 @@
+import unittest
+
+from data_structures.nodes import DoublyLinkedListNode as Node
+
+
+class TestBSTNode(unittest.TestCase):
+    def setUp(self) -> None:
+        self.vals = (1, 2, 3)
+        self.nodes = (Node(val) for val in self.vals)
+
+    def tearDown(self) -> None:
+        del self.vals
+        del self.nodes
