f7612d0d3b4f7e8d4e81b3c2e71a391e04ff0b98
Dario <darioarias12@gmail.com>
2022-12-15 22:13:07 -0500

    Testing all of the properties found in a doubly linked list node.

--- python/leetcode/tests/unit/test_dll_node.py
+++ python/leetcode/tests/unit/test_dll_node.py
@@ -11,3 +11,29 @@ class TestBSTNode(unittest.TestCase):
     def tearDown(self) -> None:
         del self.vals
         del self.nodes
+
+    def test_properties(self) -> None:
+        n_one, n_two, *_ = self.nodes
+        one, *_ = self.vals
+
+        self.assertEqual(n_one.value, one)
+        self.assertEqual(n_one.val, one)
+
+        self.assertIs(n_one.previous, None)
+        self.assertIs(n_one.prev, None)
+        self.assertIs(n_one.next, None)
+
+        n_one.next = n_two
+
+        self.assertIs(n_one.next, n_two)
+        n_two.previous = n_one
+
+        self.assertIs(n_two.previous, n_one)
+        self.assertEqual(n_two.prev, n_two.previous)
+
+        n_two.prev = None
+        self.assertIs(n_two.prev, None)
+        self.assertEqual(n_two.prev, n_two.previous)
+
+        n_one.next = None
+        self.assertIs(n_one.next, None)
